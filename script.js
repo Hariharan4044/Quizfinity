e6104ca94df76187521cc7ca462abbe1baa910e5
Hariharan <146200143+Hariharan4044@users.noreply.github.com>
2025-01-22 10:20:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const quizData = [
         a: "Central Style Sheets",
         b: "Cascading Style Sheets",
         c: "Cascading Simple Sheets",
-        d: "Cars SUVs Sailboats",
+        d: "Central Simple Sheets",
         correct: "b",
     },
     {
